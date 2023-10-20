094fd85089df98815c31f3f8abf2edc4dd835c8f
Mohamed-Ali-SOBHI <mohammedalisobhi@gmail.com>
2023-10-20 23:16:14 +0200

    MVP

--- face_detector.py
+++ face_detector.py
@@ -3,14 +3,29 @@ import cv2
 class FaceDetector:
     def __init__(self):
         self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-        self.cap = cv2.VideoCapture(0)
+        self.cap = self.get_first_available_camera()
+
+    def get_first_available_camera(self):
+        for i in range(10):  # Essayez les indices de 0 à 9
+            cap = cv2.VideoCapture(i)
+            if cap.isOpened():
+                ret, frame = cap.read()
+                if ret:  # Vérifie si la caméra est en mesure de capturer une image
+                    print(f"Caméra trouvée à l'indice {i}")
+                    return cap
+                else:
+                    cap.release()  # Libère la ressource si elle ne peut pas capturer d'image
+        raise Exception("Aucune caméra disponible")
 
     def detect_face(self):
         ret, frame = self.cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
+        if ret:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+            for (x, y, w, h) in faces:
+                return x, y, w, h
+        return 0, 0, 0, 0  # retourne des valeurs par défaut si aucune détection
 
-        for (x, y, w, h) in faces:
-            center_x, center_y = x + w // 2, y + h // 2
-            return center_x, center_y  # retourne le centre du premier visage détecté
-        return None, None  # aucun visage détecté
+    def draw_face_rectangle(self, frame, x, y, w, h):
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
--- main_app.py
+++ main_app.py
@@ -1,30 +1,47 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Canvas, ttk
+from PIL import Image, ImageTk
+import cv2
 import threading
-from face_detector import FaceDetector
-from mouse_mover import MouseMover
+from face_detector import FaceDetector  # Assurez-vous que face_detector.py est dans le même répertoire
+# from mouse_mover import MouseMover  # Uncomment this if you have the MouseMover class implemented
 
 class App:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("MVP eViacam")
-        self.label = ttk.Label(self.root, text="Cliquez pour activer")
+    def __init__(self, window):
+        self.window = window
+        self.window.title("MVP eViacam")
+
+        self.label = ttk.Label(self.window, text="Cliquez pour activer")
         self.label.grid(row=0, column=0)
-        self.button = ttk.Button(self.root, text="Activer", command=self.start_detection)
+
+        self.button = ttk.Button(self.window, text="Activer", command=self.start_detection)
         self.button.grid(row=1, column=0)
+
+        self.canvas = Canvas(self.window, width=640, height=480)
+        self.canvas.grid(row=2, column=0)
 
     def start_detection(self):
         self.label.config(text="Détection active")
         self.face_detector = FaceDetector()
-        self.mouse_mover = MouseMover()
+        # self.mouse_mover = MouseMover()  # Uncomment this if you have the MouseMover class implemented
+
         self.thread = threading.Thread(target=self.update, args=())
         self.thread.daemon = True
         self.thread.start()
 
     def update(self):
         while True:
-            x, y = self.face_detector.detect_face()
-            self.mouse_mover.move_mouse(x, y)
+            ret, frame = self.face_detector.cap.read()
+            if ret:
+                x, y, w, h = self.face_detector.detect_face()
+                self.face_detector.draw_face_rectangle(frame, x, y, w, h)
+                # self.mouse_mover.move_mouse(x, y)  # Uncomment this if you have the MouseMover class implemented
+                self.show_frame(frame)
+
+
+    def show_frame(self, frame):
+        self.image = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
+        self.canvas.create_image(0, 0, image=self.image, anchor=tk.NW)
 
 if __name__ == "__main__":
     root = tk.Tk()
