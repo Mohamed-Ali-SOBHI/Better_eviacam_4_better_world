b9ebfd7405408a568db10a1a4b1256a4452fbecb
Mohamed-Ali-SOBHI <mohammedalisobhi@gmail.com>
2023-11-07 11:40:21 +0100

    Add mediapipe face detection and camera handler, update FaceDetector class

--- face_detector.py
+++ face_detector.py
@@ -1,32 +1,40 @@
 import cv2
+import mediapipe as mp
+from camera_handler import *
 
 class FaceDetector:
-    def __init__(self):
-        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-        self.cap = self.get_first_available_camera()
-
-    def get_first_available_camera(self):
-        for i in range(10):  # Essayez les indices de 0 à 9
-            cap = cv2.VideoCapture(i)
-            if cap.isOpened():
-                ret, frame = cap.read()
-                if ret:  # Vérifie si la caméra est en mesure de capturer une image
-                    print(f"Caméra trouvée à l'indice {i}")
-                    return cap
-                else:
-                    cap.release()  # Libère la ressource si elle ne peut pas capturer d'image
-        raise Exception("Aucune caméra disponible")
+    def __init__(self, camera_handler, min_detection_confidence=0.5):
+        self.camera_handler = camera_handler
+        self.min_detection_confidence = min_detection_confidence
+        self.mp_face_detection = mp.solutions.face_detection
+        self.face_detection = self.mp_face_detection.FaceDetection(min_detection_confidence)
 
     def detect_face(self):
-        ret, frame = self.cap.read()
-        if ret:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-            confidence = 0
-            for (x, y, w, h) in faces:
+        ret, frame = self.camera_handler.get_frame()
+        if not ret:
+            print("Failed to get frame from the camera")
+            return 0, 0, 0, 0, 0.0  # Return zeros if no frame is captured
+        
+        # Convert the BGR image to RGB before processing
+        image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.face_detection.process(image_rgb)
+
+        if results.detections:
+            for detection in results.detections:
+                # Get the bounding box and confidence of the first face detected
+                bboxC = detection.location_data.relative_bounding_box
+                ih, iw, _ = frame.shape  # Image height, width
+                x, y, w, h = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                             int(bboxC.width * iw), int(bboxC.height * ih)
+                confidence = detection.score[0]
+                # Return the bounding box and confidence
                 return x, y, w, h, confidence
-        return 0, 0, 0, 0, 0  # retourne des valeurs par défaut si aucune détection
+        # If no faces are detected, return zeros
+        return 0, 0, 0, 0, 0.0
 
-    def draw_face_rectangle(self, frame, x, y, w, h):
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    def draw_face_rectangle(self, image, x, y, w, h):
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+    def close(self):
+        self.face_detection.close()
+        self.camera_handler.cap.release()
--- main_app.py
+++ main_app.py
@@ -1,47 +1,47 @@
 import tkinter as tk
 from tkinter import Canvas, ttk
 from PIL import Image, ImageTk
-import cv2
 import threading
-from face_detector import FaceDetector  # Assurez-vous que face_detector.py est dans le même répertoire
-from mouse_mover import MouseMover  # Uncomment this if you have the MouseMover class implemented
+from face_detector import FaceDetector  # Make sure face_detector.py is in the same directory
+from camera_handler import CameraHandler
+import cv2
 
 class App:
     def __init__(self, window):
         self.window = window
         self.window.title("MVP eViacam")
-
+        
         self.label = ttk.Label(self.window, text="Cliquez pour activer")
         self.label.grid(row=0, column=0)
-
+        
         self.button = ttk.Button(self.window, text="Activer", command=self.start_detection)
         self.button.grid(row=1, column=0)
-
+        
         self.canvas = Canvas(self.window, width=640, height=480)
         self.canvas.grid(row=2, column=0)
         
         self.confidence_label = ttk.Label(self.window, text="Confidence: ")
         self.confidence_label.grid(row=3, column=0)
+        
+        self.camera_handler = CameraHandler()
+        self.face_detector = FaceDetector(self.camera_handler, min_detection_confidence=0.5)
+
 
     def start_detection(self):
         self.label.config(text="Détection active")
-        self.face_detector = FaceDetector()
-        # self.mouse_mover = MouseMover()  # Uncomment this if you have the MouseMover class implemented
-
         self.thread = threading.Thread(target=self.update, args=())
         self.thread.daemon = True
         self.thread.start()
 
     def update_confidence(self, confidence):
         self.confidence_label.config(text=f"Confiance: {confidence:.2f}")
-        
+
     def update(self):
         while True:
-            ret, frame = self.face_detector.cap.read()
+            ret, frame = self.camera_handler.get_frame()  # Use the CameraHandler's method here
             if ret:
                 x, y, w, h, confidence = self.face_detector.detect_face()
                 self.face_detector.draw_face_rectangle(frame, x, y, w, h)
-                # self.mouse_mover.move_mouse(x, y)  # Uncomment this if you have the MouseMover class implemented
                 self.show_frame(frame)
                 self.update_confidence(confidence)
 
