131c6655bfcaf2e170669e1ea7fa6a1472fe94e0
Mohamed-Ali-SOBHI <mohammedalisobhi@gmail.com>
2024-02-03 14:13:40 +0100

    Update UI and add toolbar buttons

--- Better_eviacam_4_better_world/app_ui.py
+++ Better_eviacam_4_better_world/app_ui.py
@@ -11,12 +11,16 @@ from app_settings_ui import SettingsWindow
 from mouse_mover import MouseMover
 
 class App:
-    def __init__(self, window):
+    def __init__(self, window: tk.Tk):
         Config.load_settings()
         print('Config loaded')
         
         self.window = window
         self.window.title('BETTER EVIACAM 4 BETTER WORLD')
+        
+        self.canvas = Canvas(self.window, width=640, height=480)
+        self.confidence_label = ttk.Label(self.window, text='Confiance: ')
+
         self.init_ui()
         self.init_detectors()
         print('Detectors initialized')
@@ -29,16 +33,23 @@ class App:
         print('Mouse mover initialized')
 
     def init_ui(self):
+        self.toolbar = self.create_toolbar()
+        self.toolbar.grid(row=0, column=0, columnspan=2, sticky="ew")
+        self.create_main_buttons()
 
-        self.canvas = Canvas(self.window, width=Config.CANVAS_WIDTH, height=Config.CANVAS_HEIGHT)
-        self.canvas.grid(row=1, column=0)
+    def create_toolbar(self) -> ttk.Frame:
+        toolbar = ttk.Frame(self.window)
+        button_texts = ["Pas de clic", "Clic gauche", "Clic milieu", "Clic droit", "Clic maintenu", "Double clic", "Ouvrir la fenêtre principale"]
+        for text in button_texts:
+            btn = ttk.Button(toolbar, text=text)
+            btn.pack(side="left", padx=2, pady=2)
+        return toolbar
 
-        self.confidence_label = ttk.Label(self.window, text='Confiance: ')
+    def create_main_buttons(self):
+        self.canvas.grid(row=1, column=0, columnspan=2)
         self.confidence_label.grid(row=2, column=0)
-
         self.switch_button = ttk.Button(self.window, text='Switch to Iris Detection', command=self.switch_mode)
         self.switch_button.grid(row=3, column=0)
-
         self.settings_button = ttk.Button(self.window, text="Paramètres", command=self.open_settings)
         self.settings_button.grid(row=4, column=0)
 
@@ -54,7 +65,7 @@ class App:
         self.thread = threading.Thread(target=self.update, args=(), daemon=True)
         self.thread.start()
 
-    def update_confidence(self, confidence):
+    def update_confidence(self, confidence: float):
         self.confidence_label.config(text=f"Confiance: {confidence:.2f}")
 
     def update(self):
@@ -71,17 +82,13 @@ class App:
         self.show_frame(frame)
 
     def process_face_detection(self, frame):
-        """
-        Process face detection and move the mouse accordingly.
-        """
         detection_result = self.face_detector.detect_face()
         if detection_result:
             x, y, w, h, confidence = detection_result
             if confidence > Config.MIN_DETECTION_CONFIDENCE:
                 self.face_detector.draw_face_rectangle(frame, x, y, w, h)
                 self.update_confidence(confidence)
-                # Move the mouse to the converted screen coordinates
-                self.mouse_mover.move_mouse(x, y)
+                # self.mouse_mover.move_mouse(x, y)
             
     def process_iris_detection(self, frame):
         l_cx, l_cy, l_radius, r_cx, r_cy, r_radius = self.iris_detector.detect_iris()
@@ -90,7 +97,7 @@ class App:
             self.iris_detector._draw_iris(frame, r_cx, r_cy, r_radius)
             avg_x = (l_cx + r_cx) // 2
             avg_y = (l_cy + r_cy) // 2
-            #self.mouse_mover.move_mouse(avg_x, avg_y)
+            # self.mouse_mover.move_mouse(avg_x, avg_y)
 
     def show_frame(self, frame):
         self.image = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
@@ -100,6 +107,4 @@ class App:
         self.detection_mode = 'iris' if self.detection_mode == 'face' else 'face'
         buttonText = "Switch to Face Detection" if self.detection_mode == 'iris' else "Switch to Iris Detection"
         self.switch_button.config(text=buttonText)
-
-
-
+        
